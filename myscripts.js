63df31a8e0a2bc4b3adead90bed5c08d14bbaff8
pac <sunpochin@gmail.com>
2023-02-09 17:18:44 +0800

    adjust circles

--- myscripts.js
+++ myscripts.js
@@ -1,12 +1,16 @@
 let d = new Date();
 // document.body.innerHTML = "<h1>Today's date is " + d + '</h1>';
 
-let card = document.getElementById('card');
+let rating = document.getElementById('rating');
+let result = document.getElementById('result');
+
+
 let button = document.getElementById('button');
 button.addEventListener(
 	'click',
 	function (e) {
-		alert('<事件1>Hello，您好！');
+		rating.style.display = "none";
+    result.style.display = "block";
 	},
 	false
 );
